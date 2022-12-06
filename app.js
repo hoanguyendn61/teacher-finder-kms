f3fd29e48fe71ac9f8c19097bdf63a661b050bb5
hoanguyendn61 <hoanguyendn61@gmail.com>
2022-12-06 16:20:25 +0700

    feat: feat: add service for authentication

--- app.js
+++ app.js
@@ -3,14 +3,38 @@ const app = express();
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const User = require("./models/user");
 const flash = require("connect-flash");
+
+const passport = require("passport");
+const methodOverride = require("method-override");
+const LocalStrategy = require("passport-local");
+
+const indexRoutes = require("./routes/index");
 mongoose.connect(process.env.DATABASE_URL);
 
 app.locals.moment = require("moment");
-
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
 app.set("view engine", "ejs");
+
 app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(
+  require("express-session")({
+    secret: "Once again blabla",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
@@ -18,6 +42,8 @@ app.use(function (req, res, next) {
   res.locals.success = req.flash("success");
   next();
 });
+
+app.use("/", indexRoutes);
 
 //=============================================================================
 //Starts the server
---
+++ routes/index.js
@@ -0,0 +1,442 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+var Teacher = require("../models/teacher");
+var async = require("async");
+var nodemailer = require("nodemailer");
+var crypto = require("crypto");
+var middleware = require("../middleware");
+
+//*************************
+// Image Uploader
+//*************************
+
+var multer = require("multer");
+var storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+var imageFilter = function (req, file, cb) {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter });
+
+var cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//INDEX - show all Teachers
+router.get("/", function (req, res) {
+  res.render("landing");
+});
+
+//=============================================================================
+// AUTH ROUTES
+
+router.get("/register", function (req, res) {
+  res.render("register", { page: "register" });
+});
+
+router.get("/regadm", function (req, res) {
+  res.render("regadm", { page: "regadm" });
+});
+
+//confirmation and resend
+
+//HANDLE SIGNUP LOGIC
+
+router.post("/register", function (req, res, next) {
+  var newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+  });
+
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      checkErr = true;
+      return res.render("register", { error: err.message });
+    } else {
+      async.waterfall(
+        [
+          function (done) {
+            crypto.randomBytes(20, function (err, buf) {
+              var token = buf.toString("hex");
+              done(err, token);
+            });
+          },
+          function (token, done) {
+            User.findOne({ email: req.body.email }, function (err, user) {
+              if (err) {
+                req.flash("error", "Something went wrong.");
+                res.redirect("/");
+              }
+              if (!user) {
+                req.flash(
+                  "error",
+                  "No account with that email address exists."
+                );
+                return res.redirect("/");
+              }
+
+              user.isVerifiedToken = token;
+              user.isVerifiedExpires = Date.now() + 3600000 * 24; // 24 hour
+
+              user.save(function (err) {
+                done(err, token, user);
+              });
+            });
+          },
+          function (token, user, done) {
+            res.redirect("/login");
+          },
+        ],
+        function (err) {
+          if (err) return next(err);
+          res.redirect("/");
+        }
+      );
+    }
+  });
+});
+
+router.post("/regadm", upload.single("image"), function (req, res) {
+  cloudinary.v2.uploader.upload(req.file.path, function (err, result) {
+    if (err) {
+      req.flash("error", err.message);
+      return res.redirect("back");
+    }
+    var newUser = new User({
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      avatar: result.secure_url,
+      imageId: result.public_id,
+      about: req.body.about,
+    });
+
+    if (req.body.adminCode === "secretcode123") {
+      newUser.isAdmin = true;
+      newUser.isVerified = true;
+    }
+
+    User.register(newUser, req.body.password, function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("register", { error: err.message });
+      }
+      passport.authenticate("local")(req, res, function () {
+        req.flash("success", "Welcome to TF " + req.body.username);
+        res.redirect("/teachers");
+      });
+    });
+  });
+});
+
+//SHOW LOGIN FORM
+
+router.get("/login", function (req, res) {
+  res.render("login", { page: "login" });
+});
+
+// HANDLING LOGIN LOGIC
+
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      req.flash("error", "Check entered data!");
+      res.redirect("/login");
+    }
+    if (!user) {
+      req.flash("error", "Check entered data!");
+      return res.redirect("/login");
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Successfully logged in, welcome to TF!");
+      var redirectTo = req.session.redirectTo
+        ? req.session.redirectTo
+        : "/teachers";
+      delete req.session.redirectTo;
+      res.redirect(redirectTo);
+    });
+  })(req, res, next);
+});
+
+// LOGOUT ROUTE
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Logged you out!");
+    res.redirect("/teachers");
+  });
+});
+
+// Middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+// forgot password
+router.get("/forgot", function (req, res) {
+  res.render("forgot");
+});
+
+router.post("/forgot", function (req, res, next) {
+  async.waterfall(
+    [
+      function (done) {
+        crypto.randomBytes(20, function (err, buf) {
+          var token = buf.toString("hex");
+          done(err, token);
+        });
+      },
+      function (token, done) {
+        User.findOne({ email: req.body.email }, function (err, user) {
+          if (err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("/");
+          }
+          if (!user) {
+            req.flash("error", "No account with that email address exists.");
+            return res.redirect("/forgot");
+          }
+
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+          user.save(function (err) {
+            done(err, token, user);
+          });
+        });
+      },
+    ],
+    function (err) {
+      if (err) return next(err);
+      res.redirect("/forgot");
+    }
+  );
+});
+
+router.get("/reset/:token", function (req, res) {
+  User.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() },
+    },
+    function (err, user) {
+      if (err) {
+        req.flash("error", "Something went wrong.");
+        res.redirect("/");
+      }
+      if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/forgot");
+      }
+      res.render("reset", { token: req.params.token });
+    }
+  );
+});
+
+router.post("/reset/:token", function (req, res) {
+  async.waterfall(
+    [
+      function (done) {
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() },
+          },
+          function (err, user) {
+            if (err) {
+              req.flash("error", "Something went wrong.");
+              res.redirect("/");
+            }
+            if (!user) {
+              req.flash(
+                "error",
+                "Password reset token is invalid or has expired."
+              );
+              return res.redirect("back");
+            }
+            if (req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, function (err) {
+                if (err) {
+                  req.flash("error", "Something went wrong.");
+                  res.redirect("/");
+                }
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save(function (err) {
+                  if (err) {
+                    req.flash("error", "Something went wrong.");
+                    res.redirect("/");
+                  }
+                  req.logIn(user, function (err) {
+                    done(err, user);
+                  });
+                });
+              });
+            } else {
+              req.flash("error", "Passwords do not match.");
+              return res.redirect("back");
+            }
+          }
+        );
+      },
+    ],
+    function (err) {
+      if (err) {
+        req.flash("error", "Something went wrong.");
+        res.redirect("/");
+      }
+      res.redirect("/teachers");
+    }
+  );
+});
+
+// USER PROFILE
+router.get("/users/:id", function (req, res) {
+  User.findById(req.params.id, function (err, foundUser) {
+    if (err) {
+      req.flash("error", "Something went wrong.");
+      return res.redirect("/");
+    }
+    Teacher.find()
+      .where("author.id")
+      .equals(foundUser._id)
+      .exec(function (err, teachers) {
+        if (err) {
+          req.flash("error", "Something went wrong.");
+          return res.redirect("/");
+        }
+        res.render("users/show", { user: foundUser, teachers: teachers });
+      });
+  });
+});
+
+// verify password
+router.get("/verify", function (req, res) {
+  res.render("verify");
+});
+
+router.post("/verify", function (req, res, next) {
+  async.waterfall(
+    [
+      function (done) {
+        crypto.randomBytes(20, function (err, buf) {
+          var token = buf.toString("hex");
+          done(err, token);
+        });
+      },
+      function (token, done) {
+        User.findOne({ email: req.body.email }, function (err, user) {
+          if (err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("/");
+          }
+          if (!user) {
+            req.flash("error", "No account with that email address exists.");
+            return res.redirect("/verify");
+          }
+
+          user.isVerifiedToken = token;
+          user.isVerifiedExpires = Date.now() + 3600000 * 24; // 24 hour
+
+          user.save(function (err) {
+            done(err, token, user);
+          });
+        });
+      },
+    ],
+    function (err) {
+      if (err) return next(err);
+      res.redirect("/forgot");
+    }
+  );
+});
+
+router.get("/verified/:token", function (req, res) {
+  var checkErr = false;
+  var controlCheck = false;
+
+  if (!checkErr) {
+    async.waterfall(
+      [
+        function (done) {
+          User.findOne(
+            {
+              isVerified: false,
+              isVerifiedToken: req.params.token,
+              isVerifiedExpires: { $gt: Date.now() },
+            },
+            function (err, user) {
+              if (err) {
+                req.flash("error", "Something went wrong.");
+                res.redirect("/");
+              }
+              if (!user) {
+                req.flash(
+                  "error",
+                  "Verification token is invalid, has expired or the account is already verified."
+                );
+                return res.redirect("/teachers");
+              }
+              if ("a" === "a") {
+                if (err) {
+                  req.flash("error", "Something went wrong.");
+                  res.redirect("/");
+                }
+                user.isVerifiedToken = undefined;
+                user.isVerifiedExpires = undefined;
+                user.isVerified = true;
+
+                user.save(function (err) {
+                  if (err) {
+                    req.flash("error", "Something went wrong.");
+                    res.redirect("/");
+                  }
+                  req.logIn(user, function (err) {
+                    done(err, user);
+                  });
+                });
+              } else {
+                req.flash("error", "Passwords do not match.");
+                return res.redirect("back");
+              }
+            }
+          );
+        },
+      ],
+      function (err) {
+        if (err) {
+          req.flash("error", "Something went wrong.");
+          res.redirect("/");
+        }
+        res.redirect("/teachers");
+      }
+    );
+  }
+});
+
+module.exports = router;
