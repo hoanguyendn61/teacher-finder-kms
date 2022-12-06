be040be31bb058f33042f88c2bd15cafdb07d0f2
Hoa Nguyen <hoanguyendn61@gmail.com>
2022-12-07 00:17:27 +0700

    feat: add eslint, prettier

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const User = require("./models/user");
-const flash = require("connect-flash");
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const flash = require('connect-flash');
 
-const passport = require("passport");
-const methodOverride = require("method-override");
-const LocalStrategy = require("passport-local");
+const passport = require('passport');
+const methodOverride = require('method-override');
+const LocalStrategy = require('passport-local');
 
-const indexRoutes = require("./routes/index");
+const indexRoutes = require('./routes/index');
 mongoose.connect(process.env.DATABASE_URL);
 
-app.locals.moment = require("moment");
-app.use(methodOverride("_method"));
+app.locals.moment = require('moment');
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(flash());
 
 // PASSPORT CONFIGURATION
 app.use(
-  require("express-session")({
-    secret: "Once again blabla",
+  require('express-session')({
+    secret: 'Once again blabla',
     resave: false,
     saveUninitialized: false,
   })
@@ -38,16 +38,16 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
   next();
 });
 
-app.use("/", indexRoutes);
+app.use('/', indexRoutes);
 
 //=============================================================================
 //Starts the server
 
 app.listen(process.env.PORT, process.env.IP, function () {
-  console.log("The server has started! at ", process.env.PORT);
+  console.log('The server has started! at ', process.env.PORT);
 });
